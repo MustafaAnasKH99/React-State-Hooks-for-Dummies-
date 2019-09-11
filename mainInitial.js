90bd33c21a060e08c32b8b27f5ada9a94745a7d8
Mustafa Anas <khmostafa4@gmail.com>
2019-09-11 08:55:56 +0300

    Duplicate Files

--- mainInitial.js
+++ mainInitial.js
@@ -1,6 +1,7 @@
 const locStor = localStorage.getItem('student')
 if(locStor == null){
     console.log('Storage is Empty')
+    document.getElementById('li').append('Storage is Empty');
 } else {
     let student = localStorage.getItem('student')
     document.getElementById('li').append(student);
@@ -8,7 +9,7 @@ if(locStor == null){
 
 const run = (val) => {
     let newStudent = document.createTextNode(val)
-    document.getElementById('li').append(newStudent);
+    // document.getElementById('li').append(newStudent);
     localStorage.setItem('student', val)
 }
 
